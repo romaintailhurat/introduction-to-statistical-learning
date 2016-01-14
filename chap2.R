c6e8f12f0e2deb9409b11a14560944a72d0886b1
romaintailhurat <romain.tailhurat@gmail.com>
2016-01-14 22:55:22 +0100

    Starting chapter 2

---
+++ chap2.R
@@ -0,0 +1,14 @@
+# Vectors
+x = c(1,2,3)
+x
+
+# Creating sequence
+y = seq(from=2, length=3, by=4)
+y
+?seq # ? is use for documentation
+x + y # vector addition suppose vectors of the same length
+x / y
+x ^ y
+x[2:3]
+x[-2] # removing 2nd element
+
