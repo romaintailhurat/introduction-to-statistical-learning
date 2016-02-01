c4d84c2f9c498da394b1572b847140b1c3bbb5b6
romaintailhurat <romain.tailhurat@gmail.com>
2016-02-01 21:51:24 +0100

    Multiple linear regression

--- regression.R
+++ regression.R
@@ -16,4 +16,15 @@ confint(fit1) #confidence interval
 predict(fit1, data.frame(lstat = c(5, 10, 15)), interval = "confidence")
 
 ### multiple linear regression
+fit2 = lm(medv~lstat+age, data = Boston)
+summary(fit2) # age is significant but less than lstat in this model
+fit3 = lm(medv~., data = Boston) # use all the other variables as predictors
+summary(fit3) # here, age is no more a significant predictor
+par(mfrow=c(2, 2)) # plotting a two by two layouts
+plot(fit3)
+fit4 =  update(fit3, ~.-age-indus) # updating the previous model
+summary(fit4)
+
+
+### Non linear terms and interactions
 
